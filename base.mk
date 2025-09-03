c9d901422407e9caa8d9ca4ad9c0cb1c24890cd8
Rama Mohan Mekala <rmekala@qti.qualcomm.com>
2025-09-03 11:07:16 +0530

    qssi_tiny_32go : Enablement of qssi_tiny_32go project for A16
    - Reused relevant content from qssi_32go project
    - Fixed compilation issues for qssi_tiny_32go lunch target
    - Updated project to align with A16 codebase
    
    Change-Id: I3dfa05e751f8b67043b88f3cdd162e442636c794
    (cherry picked from commit b879df4d0f2c40456dce187158f2030fa68ba4fb)

--- base.mk
+++ base.mk
@@ -181,7 +181,7 @@ TINY_ALSA_TEST_APPS := tinyplay
 TINY_ALSA_TEST_APPS += tinycap
 TINY_ALSA_TEST_APPS += tinymix
 TINY_ALSA_TEST_APPS += tinypcminfo
-TINY_ALSA_TEST_APPS += cplay
+#TINY_ALSA_TEST_APPS += cplay
 
 #AMPLOADER
 AMPLOADER := amploader
@@ -730,7 +730,6 @@ WPA += hs20-osu-client
 #ZLIB
 ZLIB := gzip
 ZLIB += libunz
-ZLIB_HOST := minigzip
 
 #Charger
 CHARGER := charger
@@ -884,14 +883,12 @@ PRODUCT_PACKAGES += $(QRGND)
 PRODUCT_PACKAGES += $(UPDATER)
 PRODUCT_PACKAGES += $(WPA)
 PRODUCT_PACKAGES += $(ZLIB)
-PRODUCT_HOST_PACKAGES += $(ZLIB_HOST)
 PRODUCT_PACKAGES += $(VT_JNI)
 PRODUCT_PACKAGES += $(VT_QTI_PERMISSIONS)
 PRODUCT_PACKAGES += $(CRDA)
 PRODUCT_PACKAGES += $(WLAN)
 PRODUCT_PACKAGES += $(FD_LEAK)
 
-PRODUCT_PACKAGES += move_wifi_data.sh
 PRODUCT_PACKAGES += librs_jni
 PRODUCT_PACKAGES += libion
 
@@ -947,11 +944,6 @@ endif
 # OEM Unlock reporting
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.oem_unlock_supported=1
-
-
-# VNDK-SP:
-PRODUCT_PACKAGES += \
-    vndk-sp \
 
 TARGET_FS_CONFIG_GEN := device/qcom/qssi_tiny_32go/config.fs
 ifeq ($(TARGET_HAS_LOW_RAM),true)
