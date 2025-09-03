b879df4d0f2c40456dce187158f2030fa68ba4fb
Rama Mohan Mekala <rmekala@qti.qualcomm.com>
2025-09-03 11:07:16 +0530

    qssi_tiny_32go : Enablement of qssi_tiny_32go project for A16
    - Reused relevant content from qssi_32go project
    - Fixed compilation issues for qssi_tiny_32go lunch target
    - Updated project to align with A16 codebase
    
    Change-Id: I3dfa05e751f8b67043b88f3cdd162e442636c794

--- qssi_tiny_32go.mk
+++ qssi_tiny_32go.mk
@@ -8,10 +8,6 @@ TARGET_BOOTLOADER_BOARD_NAME := qssi_tiny_32go
 # Skip VINTF checks for kernel configs since we do not have kernel source
 PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS := false
 
-# Enable product partition Native I/F. It is automatically set to current if
-# the shipping API level for the target is greater than 29.
-PRODUCT_PRODUCT_VNDK_VERSION := current
-
 # Mismatch in the uses-library tags between build system and the manifest leads
 # to soong APK manifest_check tool errors. Enable the flag to fix this.
 RELAX_USES_LIBRARY_CHECK := true
@@ -151,7 +147,7 @@ PRODUCT_DEVICE := $(VENDOR_QTI_DEVICE)
 PRODUCT_BRAND := qti
 PRODUCT_MODEL := qssi system image for arm64
 
-PRODUCT_EXTRA_VNDK_VERSIONS := 30 33
+PRODUCT_EXTRA_VNDK_VERSIONS := 30 33 34
 
 #Initial bringup flags
 TARGET_USES_AOSP := false
@@ -164,6 +160,8 @@ TARGET_USES_NQ_NFC := true
 # default is nosdcard, S/W button enabled in resource
 PRODUCT_CHARACTERISTICS := nosdcard
 BOARD_FRP_PARTITION_NAME := frp
+
+PRODUCT_PACKAGES += qspa_system.rc qspa_default.rc
 
 #Android EGL implementation
 PRODUCT_PACKAGES += libGLES_android
@@ -272,9 +270,6 @@ endif
 PRODUCT_FULL_TREBLE_OVERRIDE := true
 PRODUCT_VENDOR_MOVE_ENABLED := true
 PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
-
-#Enable vndk-sp Libraries
-PRODUCT_PACKAGES += vndk_package
 
 PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE:=true
 
